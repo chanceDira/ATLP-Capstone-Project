d04b26ea17110fd568a1bb4bed278a7d03b6cb14
chanceDira <chancedesire60@gmail.com>
2021-12-07 22:20:18 +0200

    Finishing with toggle menu

--- javascript/index.js
+++ javascript/index.js
@@ -5,3 +5,12 @@ function validateForm() {
       return false;
     }
   }
+
+  function toggle() {
+    var x = document.getElementById("card_menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
