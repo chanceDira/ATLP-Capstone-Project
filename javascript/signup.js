2f887cbeb9ef2f6bb24f8f14537103fe23f4e3a7
chanceDira <chancedesire60@gmail.com>
2021-12-13 09:50:41 +0200

    Worked on articles landing and single articles

--- javascript/signup.js
+++ javascript/signup.js
@@ -12,29 +12,7 @@ const password = document.getElementById('signup_password_input');
          validateInputs();
      });
      
-     const setError = (element, message) =>{
-         const inputControl = element.parentElement;
-         const errorDisplay = inputControl.querySelector('.error');
-
-        errorDisplay.innerText = message;
-        inputControl.classList.add('error');
-        inputControl.classList.remove('success');
-
-    }
-    const setSuccess = element =>{
-        const inputControl = element.parentElement;
-        const errorDisplay = inputControl.querySelector('.error');
-        
-        errorDisplay.innerText = '';
-        inputControl.classList.add('success');
-        inputControl.classList.remove('error');
-
-    }
-
-const isEmailValid = (email) => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-};
+     
 
     const validateInputs = () =>{
         console.log(password.value);
@@ -71,3 +49,27 @@ const isEmailValid = (email) => {
         }
   
     };
+
+    const setError = (element, message) =>{
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+
+       errorDisplay.innerText = message;
+       inputControl.classList.add('error');
+       inputControl.classList.remove('success');
+
+   }
+   const setSuccess = element =>{
+       const inputControl = element.parentElement;
+       const errorDisplay = inputControl.querySelector('.error');
+       
+       errorDisplay.innerText = '';
+       inputControl.classList.add('success');
+       inputControl.classList.remove('error');
+
+   }
+
+const isEmailValid = (email) => {
+   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   return re.test(email);
+};
